91e024569f61c2f38567389c9019d2a50e0c594c
Vadim Filimonov <vvfilimonov@avito.ru>
2023-02-11 10:43:12 +0300

    Start to use env

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	env "github.com/caarlos0/env/v6"
 
 	"github.com/VadimFilimonov/urlshortener/internal/handler"
 	"github.com/VadimFilimonov/urlshortener/internal/storage"
 	"github.com/go-chi/chi/v5"
 )
 
+type Config struct {
+	ServerAddress string `env:"SERVER_ADDRESS"`
+	BaseURL       string `env:"BASE_URL"`
+}
+
 func main() {
+	config := Config{}
+	err := env.Parse(&config)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(config)
+
 	r := chi.NewRouter()
 	data := storage.New()
+
 	r.Get("/{shortURL}", handler.New(data))
 	r.Post("/", handler.New(data))
 	r.Post("/api/shorten", handler.NewShorten(data))
-	http.ListenAndServe(":8080", r)
+	err = http.ListenAndServe(config.ServerAddress+":8080", r)
+
+	if err != nil {
+		panic(err)
+	}
 }
