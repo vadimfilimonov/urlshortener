030c78d838269288f95e2cd76cec782201642c3e
Vadim Filimonov <vvfilimonov@avito.ru>
2023-02-18 15:15:08 +0300

    Add config package

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,50 +3,20 @@ package main
 import (
 	"bytes"
 	"compress/gzip"
-	"flag"
 	"io"
-	"log"
 	"net/http"
 	"strings"
 
-	env "github.com/caarlos0/env/v6"
-
+	"github.com/VadimFilimonov/urlshortener/internal/config"
 	"github.com/VadimFilimonov/urlshortener/internal/handler"
 	"github.com/VadimFilimonov/urlshortener/internal/storage"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-type Config struct {
-	ServerAddress   string `env:"SERVER_ADDRESS"`
-	BaseURL         string `env:"BASE_URL"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH"`
-}
-
 func main() {
-	config := Config{}
-	err := env.Parse(&config)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	serverAddress := flag.String("a", "localhost:8080", "адрес запуска HTTP-сервера")
-	BaseURL := flag.String("b", "http://localhost:8080", "базовый адрес результирующего сокращённого URL")
-	FileStoragePath := flag.String("f", "", "путь до файла с сокращёнными URL")
-	flag.Parse()
-
-	if config.ServerAddress == "" {
-		config.ServerAddress = *serverAddress
-	}
-
-	if config.BaseURL == "" {
-		config.BaseURL = *BaseURL
-	}
-
-	if config.FileStoragePath == "" {
-		config.FileStoragePath = *FileStoragePath
-	}
+	config := config.New()
+	config.Parse()
 
 	r := chi.NewRouter()
 	r.Use(decompressMiddleware)
@@ -56,7 +26,7 @@ func main() {
 	r.Get("/{shortURL}", handler.New(data, config.BaseURL))
 	r.Post("/", handler.New(data, config.BaseURL))
 	r.Post("/api/shorten", handler.NewShorten(data, config.BaseURL))
-	err = http.ListenAndServe(config.ServerAddress, r)
+	err := http.ListenAndServe(config.ServerAddress, r)
 
 	if err != nil {
 		panic(err)
