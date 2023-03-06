3fd7d04c8b8765ab8f79eb251ac4395b1e20689a
Vadim Filimonov <philimonovvadim@gmail.com>
2023-03-07 01:29:28 +0300

    Change migration file

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE urls
 (
   id serial not null unique,
-  user_id varchar(255) not null unique,
-  shorten_url varchar(255) not null unique,
+  user_id varchar(255) not null,
+  shorten_url varchar(255) not null,
   original_url varchar(255) not null
 );
