970c594850f3f3c3a5c12dcfda70ba4ced1fcb8a
Vadim Filimonov <philimonovvadim@gmail.com>
2023-03-07 18:42:34 +0300

    Update migration file; original url must be unique

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -3,5 +3,5 @@ CREATE TABLE urls
   id serial not null unique,
   user_id varchar(255) not null,
   shorten_url varchar(255) not null,
-  original_url varchar(255) not null
+  original_url varchar(255) not null unique
 );
